ee95c095c1252411128703959654d40489fde6e7
Ali Rizvi-Santiago <arizvisa@gmail.com>
2018-09-10 06:10:58 -0500

    Removed a couple of combinators out of posix/.pythonrc.py that aren't prefixed with "f".

--- posix/.pythonrc.py
+++ posix/.pythonrc.py
@@ -16,9 +16,9 @@ map(sys.path.append, __import__('glob').iglob(os.path.join(user.home.replace('\\
 
 ## some functional primitives in the default namespace
 # box any specified arguments
-fbox = fboxed = box = boxed = lambda *a: a
+fbox = fboxed = lambda *a: a
 # return a closure that executes ``f`` with the arguments unboxed.
-funbox = unbox = lambda f, *a, **k: lambda *ap, **kp: f(*(a + builtins.reduce(operator.add, builtins.map(builtins.tuple, ap), ())), **builtins.dict(k.items() + kp.items()))
+funbox = lambda f, *a, **k: lambda *ap, **kp: f(*(a + builtins.reduce(operator.add, builtins.map(builtins.tuple, ap), ())), **builtins.dict(k.items() + kp.items()))
 # return a closure that will check that its argument is an instance of ``type``.
 finstance = lambda *type: frpartial(builtins.isinstance, type)
 # return a closure that will check if its argument has an item ``key``.
@@ -26,7 +26,7 @@ fhasitem = fitemQ = lambda key: fcompose(fcatch(frpartial(operator.getitem, key)
 # return a closure that will check if its argument has an ``attribute``.
 fhasattr = fattrQ = lambda attribute: frpartial(builtins.hasattr, attribute)
 # return a closure that always returns ``object``.
-fconstant = fconst = falways = always = lambda object: lambda *a, **k: object
+fconstant = fconst = falways = lambda object: lambda *a, **k: object
 # a closure that returns it's argument always
 fpassthru = fpass = fidentity = fid = lambda object: object
 # a closure that returns a default value if its object is false-y
@@ -34,7 +34,7 @@ fdefault = lambda default: lambda object: object or default
 # return the first, second, or third item of a box.
 first, second, third, last = operator.itemgetter(0), operator.itemgetter(1), operator.itemgetter(2), operator.itemgetter(-1)
 # return a closure that executes a list of functions one after another from left-to-right
-fcompose = compose = lambda *f: builtins.reduce(lambda f1, f2: lambda *a: f1(f2(*a)), builtins.reversed(f))
+fcompose = lambda *f: builtins.reduce(lambda f1, f2: lambda *a: f1(f2(*a)), builtins.reversed(f))
 # return a closure that executes function ``f`` whilst discarding any extra arguments
 fdiscard = lambda f: lambda *a, **k: f()
 # return a closure that executes function ``crit`` and then returns/executes ``f`` or ``t`` based on whether or not it's successful.
@@ -53,7 +53,7 @@ def flazy(f, *a, **k):
     return lazy
 fmemo = flazy
 # return a closure with the function's arglist partially applied
-fpartial = partial = functools.partial
+fpartial = functools.partial
 # return a closure that applies the provided arguments to the function ``f``.
 fapply = lambda f, *a, **k: lambda *ap, **kp: f(*(a+ap), **builtins.dict(k.items() + kp.items()))
 # return a closure that will use the specified arguments to call the provided function.
@@ -61,7 +61,7 @@ fcurry = lambda *a, **k: lambda f, *ap, **kp: f(*(a+ap), **builtins.dict(k.items
 # return a closure that applies the initial arglist to the end of function ``f``.
 frpartial = lambda f, *a, **k: lambda *ap, **kp: f(*(ap + builtins.tuple(builtins.reversed(a))), **builtins.dict(k.items() + kp.items()))
 # return a closure that applies the arglist to function ``f`` in reverse.
-freversed = frev = lambda f, *a, **k: lambda *ap, **kp: f(*builtins.reversed(a + ap), **builtins.dict(k.items() + kp.items()))
+freversed = freverse = lambda f, *a, **k: lambda *ap, **kp: f(*builtins.reversed(a + ap), **builtins.dict(k.items() + kp.items()))
 # return a closure that executes function ``f`` and includes the caught exception (or None) as the first element in the boxed result.
 def fcatch(f, *a, **k):
     def fcatch(*a, **k):
@@ -70,21 +70,21 @@ def fcatch(f, *a, **k):
     return functools.partial(fcatch, *a, **k)
 fexc = fexception = fcatch
 # boolean inversion of the result of a function
-fcomplement = fnot = complement = frpartial(fcompose, operator.not_)
+fcomplement = fnot = frpartial(fcompose, operator.not_)
 # converts a list to an iterator, or an iterator to a list
-ilist, liter = compose(builtins.list, builtins.iter), compose(builtins.iter, builtins.list)
+ilist, liter = fcompose(builtins.list, builtins.iter), fcompose(builtins.iter, builtins.list)
 # converts a tuple to an iterator, or an iterator to a tuple
-ituple, titer = compose(builtins.tuple, builtins.iter), compose(builtins.iter, builtins.tuple)
+ituple, titer = fcompose(builtins.tuple, builtins.iter), fcompose(builtins.iter, builtins.tuple)
 # take ``count`` number of elements from an iterator
-itake = lambda count: compose(builtins.iter, fmap(*(builtins.next,)*count), builtins.tuple)
+itake = lambda count: fcompose(builtins.iter, fmap(*(builtins.next,)*count), builtins.tuple)
 # get the ``nth`` element from an iterator
-iget = lambda count: compose(builtins.iter, fmap(*(builtins.next,)*(count)), builtins.tuple, operator.itemgetter(-1))
+iget = lambda count: fcompose(builtins.iter, fmap(*(builtins.next,)*(count)), builtins.tuple, operator.itemgetter(-1))
 # copy from itertools
 imap, ifilter, ichain, izip = itertools.imap, itertools.ifilter, itertools.chain, itertools.izip
 # count number of elements of a container
-count = compose(builtins.iter, builtins.list, builtins.len)
+count = fcompose(builtins.iter, builtins.list, builtins.len)
 
 __all__ = ['functools', 'operator', 'itertools', 'types', 'builtins']
 __all__+= ['first', 'second', 'third', 'last']
-__all__+= ['partial', 'fpartial', 'imap', 'ifilter', 'ichain', 'izip']
-__all__+= filter( compose(fpartial(operator.getitem,locals()), finstance(types.FunctionType)), locals())
+__all__+= ['fpartial', 'imap', 'ifilter', 'ichain', 'izip']
+__all__+= filter( fcompose(fpartial(operator.getitem,locals()), finstance(types.FunctionType)), locals())
