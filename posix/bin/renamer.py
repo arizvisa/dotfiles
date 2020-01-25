27b562aa8eed5b7077b512e1dc781c34455f6a8f
Ali Rizvi-Santiago <arizvisa@gmail.com>
2020-01-25 04:11:22 -0600

    Updated posix/bin/renamer.py so that it's compatible with Python3, and added a dry-run option.

--- posix/bin/renamer.py
+++ posix/bin/renamer.py
@@ -1,11 +1,12 @@
+#!/usr/bin/env python
 import sys,os
-import functools,operator,itertools
+import functools,itertools,types,builtins,operator,six
 import argparse,tempfile,logging,time
 import codecs
 logging.basicConfig(level=logging.INFO)
 editor = 'vim'
 
-executable = lambda(_): os.path.isfile(_) and os.access(_,os.X_OK)
+executable = lambda _: os.path.isfile(_) and os.access(_,os.X_OK)
 which = lambda _,envvar="PATH",extvar='PATHEXT':_ if executable(_) else iter(filter(executable,itertools.starmap(os.path.join,itertools.product(os.environ.get(envvar,os.defpath).split(os.pathsep),(_+e for e in os.environ.get(extvar,'').split(os.pathsep)))))).next()
 
 #hate python devers
@@ -21,10 +22,14 @@ EDITOR_ARGS = os.environ.get('EDITOR_ARGS', '-O2' if editor in EDITOR else '')
 FILEENCODING = 'utf-8-sig'
 
 ### code
+def sequence(iterable):
+	return [item for item in iterable]
+
 def parse_args():
 	res = argparse.ArgumentParser(description='Rename any number of files within a list of paths using an editor.', add_help=True)
 	res.add_argument('-d', dest='use_dirs', action='store_true', default=False, help='rename directories instead of files')
-	res.add_argument(dest='paths', metavar='path', nargs='*', action='store', type=unicode)
+	res.add_argument('-n', '--dry-run', dest='dry_run', action='store_true', default=True, help='perform a dry run and only print the changes')
+	res.add_argument(dest='paths', metavar='path', nargs='*', action='store', type=six.ensure_text)
 	return res.parse_args()
 
 def rename_file(a,b):
@@ -33,28 +38,39 @@ def rename_file(a,b):
 		if not os.path.isdir(parentdir):
 			logging.info("creating base directory {!r} to contain {!r}".format(parentdir,b))
 			os.makedirs(parentdir)
-	except Exception, e:
+	except Exception as e:
 		logging.warning("os.makedirs({!r}) raised {!r}".format(a,b,e))
 		return False
 
 	try:
 		result = os.rename(a,b)
-	except Exception, e:
+	except Exception as e:
 		logging.warning("os.rename({!r},{!r}) raised {!r}".format(a,b,e))
 		return False
 	logging.info("renamed {!r} to {!r}".format(a,b))
 	return True
 
+def rename_output(a,b):
+	parentdir = os.path.normpath(os.path.join(b, os.path.pardir)) if os.path.isdir(a) else os.path.dirname(b)
+	if not os.path.isdir(parentdir):
+		logging.info("creating base directory {!r} to contain {!r}".format(parentdir,b))
+		six.print_("mkdir -p \"{:s}\"".format(parentdir))
+	six.print_("mv \"{:s}\" \"{:s}\"".format(a, b))
+	logging.info("renamed {!r} to {!r}".format(a,b))
+	return True
+
 def rename(source,target):
+	F = rename_output if arguments.dry_run else rename_file
+
 	count = 0
-	for a,b in zip(source,target):
+	for a, b in zip(source, target):
 		if a != b:
-			count += int(rename_file(a,b))
+			count += int(F(a, b))
 		continue
 	return count
 
 def listing(path):
-	p = path if isinstance(path, unicode) else unicode(path, encoding=sys.getfilesystemencoding())
+	p = six.ensure_text(path, encoding=sys.getfilesystemencoding())
 	for dirpath,_,filenames in os.walk(p):
 		for name in sorted(filenames):
 			yield os.path.join(dirpath,name)
@@ -62,24 +78,25 @@ def listing(path):
 	return
 
 def dirlisting(path):
-	p = path if isinstance(path, unicode) else unicode(path, encoding=sys.getfilesystemencoding())
+	p = six.ensure_text(path, encoding=sys.getfilesystemencoding())
 	for dirpath,dirnames,_ in os.walk(p):
 		for name in sorted(dirnames):
 			yield os.path.join(dirpath,name)
 	return
 
 def edit(list):
-	list = filter(None, list)
+	list = sequence(filter(None, list))
 
 	[ logging.debug("renamer.edit(...) - found file {:d} -- {!r}".format(i,s)) for i,s in enumerate(list) ]
 
 	#hate python devers
 	with tempfile.NamedTemporaryFile(prefix='renamer.',suffix='.source', delete=not WIN32) as t1,tempfile.NamedTemporaryFile(prefix='renamer.',suffix='.destination', delete=not WIN32) as t2:
 		# really hate python devers
-		map(operator.methodcaller('close'), (t1,t2))
+		sequence(map(operator.methodcaller('close'), (t1,t2)))
 		with codecs.open(t1.name, 'w+b', encoding=FILEENCODING) as t1e, codecs.open(t2.name, 'w+b', encoding=FILEENCODING) as t2e:
-			map(operator.methodcaller('writelines', map(u'{:s}\n'.format, list)), (t1e,t2e))
-			map(operator.methodcaller('flush'), (t1e,t2e))
+			lines = sequence(map(u'{:s}\n'.format, list))
+			sequence(map(operator.methodcaller('writelines', lines), (t1e,t2e)))
+			sequence(map(operator.methodcaller('flush'), (t1e,t2e)))
 
 		logging.info("renamer.edit(...) - using source filename {!r}".format(t1.name))
 		logging.info("renamer.edit(...) - using destination filename {!r}".format(t2.name))
@@ -87,7 +104,7 @@ def edit(list):
 		message = "os.spawnv(os.P_WAIT, {!r}, [{!r}, {!r}, '--', {!r}, {!r}])".format(EDITOR, EDITOR, EDITOR_ARGS, t1.name, t2.name)
 		try:
 			result = os.spawnv(os.P_WAIT, EDITOR, [EDITOR, EDITOR_ARGS, '--', t1.name, t2.name])
-		except Exception, e:
+		except Exception as e:
 			logging.fatal("{:s} raised {!r}".format(message, e), exc_info=True)
 			raise
 		else:
@@ -95,9 +112,10 @@ def edit(list):
 
 		#really hate python devers
 		with codecs.open(t1.name, 'rb', encoding=FILEENCODING) as t1e, codecs.open(t2.name, 'rb', encoding=FILEENCODING) as t2e:
-			map(operator.methodcaller('seek', 0), (t1e,t2e))
+			sequence(map(operator.methodcaller('seek', 0), (t1e,t2e)))
 			source, destination = t1e.readlines(), t2e.readlines()
-			source, destination = map(unicode.strip, source), map(unicode.strip, destination)
+			source, destination = map(operator.methodcaller('strip'), source), map(operator.methodcaller('strip'), destination)
+			source, destination = map(sequence, (source, destination))
 
 		# restore the handles so that when we exit 'with', it won't double-close them.
 		t1,t2 = map(open, (t1.name,t2.name))
@@ -110,9 +128,9 @@ def edit(list):
 
 	if len(destination) != len(list):
 		logging.fatal("renamer.edit(...) - destination list contains a different number of entries from the source. terminating. ({:d} != {:d})".format(len(destination), len(list)))
-		return []
+		return [], []
 
-	return source,destination
+	return source, destination
 
 def main_files(*paths):
 	source = []
