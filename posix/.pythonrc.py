b6ad4dba94e9121c9e9e2627b1ec0c72ac3abcd2
Ali Rizvi-Santiago <arizvisa@gmail.com>
2020-11-17 14:37:32 -0600

    Tweaked the .pythonrc.py script under the posix directory so that it's compatible with python3.

--- posix/.pythonrc.py
+++ posix/.pythonrc.py
@@ -1,4 +1,4 @@
-import os, sys, user, logging
+import os, sys, logging
 import functools, itertools, types, builtins, operator, six
 
 # remove crlf from std{out,err} because CPython is pretty fucking stupid
@@ -7,17 +7,17 @@ if sys.platform == 'win32':
     __import__('msvcrt').setmode(sys.stderr.fileno(), os.O_BINARY) if hasattr(sys.stderr, 'fileno') else None
 
 # use the current virtualenv if it exists
-builtins._ = os.path.join(user.home.replace('\\', os.sep).replace('/', os.sep), '.python-virtualenv', 'Scripts' if __import__('platform').system() == 'Windows' else 'bin', 'activate_this.py')
+builtins._ = os.path.join(os.environ.get('HOME', os.path.expanduser('~')).replace('\\', os.sep).replace('/', os.sep), '.python-virtualenv', 'Scripts' if __import__('platform').system() == 'Windows' else 'bin', 'activate_this.py')
 if os.path.exists(builtins._): execfile(builtins._, {'__file__':builtins._})
 
 # add ~/.python/* to python module search path
-map(sys.path.append, __import__('glob').iglob(os.path.join(user.home.replace('\\', os.sep).replace('/', os.sep), '.python', '*')))
+map(sys.path.append, __import__('glob').iglob(os.path.join(os.environ.get('HOME', os.path.expanduser('~')).replace('\\', os.sep).replace('/', os.sep), '.python', '*')))
 
 ## some functional primitives in the default namespace
 # box any specified arguments
 fbox = fboxed = lambda *a: a
 # return a closure that executes ``f`` with the arguments unboxed.
-funbox = lambda f, *a, **k: lambda *ap, **kp: f(*(a + builtins.reduce(operator.add, builtins.map(builtins.tuple, ap), ())), **builtins.dict(k.items() + kp.items()))
+funbox = lambda f, *a, **k: lambda *ap, **kp: f(*(a + functools.reduce(operator.add, builtins.map(builtins.tuple, ap), ())), **builtins.dict(builtins.tuple(k.items()) + builtins.tuple(kp.items())))
 # return the first argument
 fcar = lambda *a: a[:1][0]
 # return the rest of the arguments
@@ -25,7 +25,7 @@ fcdr = lambda *a: a[1:]
 # return a closure that will check that its argument is an instance of ``type``.
 finstance = lambda *type: frpartial(builtins.isinstance, type)
 # return a closure that will check if its argument has an item ``key``.
-fhasitem = fitemQ = lambda key: fcompose(fcatch(frpartial(operator.getitem, key)), builtins.iter, builtins.next, fpartial(operator.eq, builtins.None))
+fhasitem = fitemQ = lambda key: fcompose(fcatch(frpartial(operator.getitem, key)), builtins.iter, builtins.next, fpartial(operator.eq, None))
 # return a closure that will get a particular element from an object
 fgetitem = fitem = lambda item, *default: lambda object: default[0] if default and item not in object else object[item] 
 # return a closure that will set a particular element on an object
@@ -45,7 +45,7 @@ fdefault = lambda default: lambda object: object or default
 # return the first, second, or third item of a box.
 first, second, third, last = operator.itemgetter(0), operator.itemgetter(1), operator.itemgetter(2), operator.itemgetter(-1)
 # return a closure that executes a list of functions one after another from left-to-right
-fcompose = lambda *f: builtins.reduce(lambda f1, f2: lambda *a: f1(f2(*a)), builtins.reversed(f))
+fcompose = lambda *f: functools.reduce(lambda f1, f2: lambda *a: f1(f2(*a)), builtins.reversed(f))
 # return a closure that executes function ``f`` whilst discarding any extra arguments
 fdiscard = lambda f: lambda *a, **k: f()
 # return a closure that executes function ``crit`` and then returns/executes ``f`` or ``t`` based on whether or not it's successful.
@@ -54,30 +54,30 @@ fcondition = fcond = lambda crit: lambda t, f: \
 # return a closure that takes a list of functions to execute with the provided arguments
 fmap = lambda *fa: lambda *a, **k: (f(*a, **k) for f in fa)
 #lazy = lambda f, state={}: lambda *a, **k: state[(f, a, builtins.tuple(builtins.sorted(k.items())))] if (f, a, builtins.tuple(builtins.sorted(k.items()))) in state else state.setdefault((f, a, builtins.tuple(builtins.sorted(k.items()))), f(*a, **k))
-#lazy = lambda f, *a, **k: lambda *ap, **kp: f(*(a+ap), **dict(k.items() + kp.items()))
+#lazy = lambda f, *a, **k: lambda *ap, **kp: f(*(a+ap), **dict(builtins.tuple(k.items()) + builtins.tuple(kp.items())))
 # return a memoized closure that's lazy and only executes when evaluated
 def flazy(f, *a, **k):
     sortedtuple, state = fcompose(builtins.sorted, builtins.tuple), {}
     def lazy(*ap, **kp):
-        A, K = a+ap, sortedtuple(k.items() + kp.items())
-        return state[(A, K)] if (A, K) in state else state.setdefault((A, K), f(*A, **builtins.dict(k.items()+kp.items())))
+        A, K = a + ap, sortedtuple(builtins.tuple(k.items()) + builtins.tuple(kp.items()))
+        return state[(A, K)] if (A, K) in state else state.setdefault((A, K), f(*A, **builtins.dict(builtins.tuple(k.items())+builtins.tuple(kp.items()))))
     return lazy
 fmemo = flazy
 # return a closure with the function's arglist partially applied
 fpartial = functools.partial
 # return a closure that applies the provided arguments to the function ``f``.
-fapply = lambda f, *a, **k: lambda *ap, **kp: f(*(a+ap), **builtins.dict(k.items() + kp.items()))
+fapply = lambda f, *a, **k: lambda *ap, **kp: f(*(a+ap), **builtins.dict(builtins.tuple(k.items()) + builtins.tuple(kp.items())))
 # return a closure that will use the specified arguments to call the provided function.
-fcurry = lambda *a, **k: lambda f, *ap, **kp: f(*(a+ap), **builtins.dict(k.items() + kp.items()))
+fcurry = lambda *a, **k: lambda f, *ap, **kp: f(*(a+ap), **builtins.dict(builtins.tuple(k.items()) + builtins.tuple(kp.items())))
 # return a closure that applies the initial arglist to the end of function ``f``.
-frpartial = lambda f, *a, **k: lambda *ap, **kp: f(*(ap + builtins.tuple(builtins.reversed(a))), **builtins.dict(k.items() + kp.items()))
+frpartial = lambda f, *a, **k: lambda *ap, **kp: f(*(ap + builtins.tuple(builtins.reversed(a))), **builtins.dict(builtins.tuple(k.items()) + builtins.tuple(kp.items())))
 # return a closure that applies the arglist to function ``f`` in reverse.
-freversed = freverse = lambda f, *a, **k: lambda *ap, **kp: f(*builtins.reversed(a + ap), **builtins.dict(k.items() + kp.items()))
+freverse = lambda f, *a, **k: lambda *ap, **kp: f(*builtins.reversed(a + ap), **builtins.dict(builtins.tuple(k.items()) + builtins.tuple(kp.items())))
 # return a closure that executes function ``f`` and includes the caught exception (or None) as the first element in the boxed result.
 def fcatch(f, *a, **k):
     def fcatch(*a, **k):
-        try: return builtins.None, f(*a, **k)
-        except: return sys.exc_info()[1], builtins.None
+        try: return None, f(*a, **k)
+        except: return sys.exc_info()[1], None
     return functools.partial(fcatch, *a, **k)
 fexc = fexception = fcatch
 # boolean inversion of the result of a function
@@ -91,7 +91,7 @@ itake = lambda count: fcompose(builtins.iter, fmap(*(builtins.next,)*count), bui
 # get the ``nth`` element from an iterator
 iget = lambda count: fcompose(builtins.iter, fmap(*(builtins.next,)*(count)), builtins.tuple, operator.itemgetter(-1))
 # copy from itertools
-islice, imap, ifilter, ichain, izip = itertools.islice, itertools.imap, itertools.ifilter, itertools.chain, itertools.izip
+islice, imap, ifilter, ichain, izip = itertools.islice, builtins.map, builtins.filter, itertools.chain, builtins.zip
 # count number of elements of a container
 count = fcompose(builtins.iter, builtins.list, builtins.len)
 
