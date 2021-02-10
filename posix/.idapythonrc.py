f1b24e49e73aee3100328c5f15a51a6ebe1ac4d7
Ali Rizvi-Santiago <arizvisa@gmail.com>
2021-02-10 03:50:34 -0600

    Fixed the combinators defined in posix/.pythonrc.py, and updated the posix/.idapythonrc.py file with them.

--- posix/.idapythonrc.py
+++ posix/.idapythonrc.py
@@ -70,13 +70,13 @@ def memberFromOp(st, ea, opnum):
     return st.members.add((name,offset), (int, size), offset)
 mop = memberFromOp
 
-dbname = fcompose(fmap(fbox, fcompose(fbox, first, fcondition(finstance(int))(db.offset, fdiscard(db.offset)), fbox)), funbox(itertools.chain), funbox(db.name))
-fnname = fcompose(fmap(fbox, fcompose(fbox, first, fcondition(finstance(int))(func.offset, fdiscard(func.offset)), fbox)), funbox(itertools.chain), funbox(func.name))
-selectall = fcompose(db.selectcontents, fpartial(imap, funbox(func.select)), funbox(itertools.chain))
+dbname = fcompose(fmap(fpack(fidentity), fcompose(fpack(fidentity), first, fcondition(finstance(int))(db.offset, fdiscard(db.offset)), fpack(fidentity))), funpack(itertools.chain), funpack(db.name))
+fnname = fcompose(fmap(fpack(fidentity), fcompose(fpack(fidentity), first, fcondition(finstance(int))(func.offset, fdiscard(func.offset)), fpack(fidentity))), funpack(itertools.chain), funpack(func.name))
+selectall = fcompose(db.selectcontents, fpartial(imap, funpack(func.select)), funpack(itertools.chain))
 
-has_immediate_ops = fcompose(fmap(fpartial(fpartial, ins.op_type), ins.ops_read), funbox(map), set, fmap(fcompose(len,fpartial(operator.eq, 1)), freverse(operator.contains, 'immediate')), all)
-has_register_ops = fcompose(fmap(fpartial(fpartial, ins.op_type), ins.ops_read), funbox(map), set, fmap(fcompose(len,fpartial(operator.eq, 1)), freverse(operator.contains, 'register')), all)
-previous_written = fcompose(fmap(fidentity, fcompose(fmap(fpartial(fpartial, ins.op), ins.ops_read), funbox(map), set)), tuple, fmap(fcompose(first,fbox), fcompose(second,list)), funbox(zip), iget(1), funbox(db.a.prevreg, write=1))
+has_immediate_ops = fcompose(fmap(fpartial(fpartial, ins.op_type), ins.ops_read), funpack(map), set, fmap(fcompose(len,fpartial(operator.eq, 1)), freverse(operator.contains, 'immediate')), all)
+has_register_ops = fcompose(fmap(fpartial(fpartial, ins.op_type), ins.ops_read), funpack(map), set, fmap(fcompose(len,fpartial(operator.eq, 1)), freverse(operator.contains, 'register')), all)
+previous_written = fcompose(fmap(fidentity, fcompose(fmap(fpartial(fpartial, ins.op), ins.ops_read), funpack(map), set)), tuple, fmap(fcompose(first,fpack(fidentity)), fcompose(second,list)), funpack(zip), iget(1), funpack(db.a.prevreg, write=1))
 
 freg_written = lambda reg: lambda ea: any(reg.relatedQ(ins.op(ea, i)) for i in ins.ops_write(ea) if ins.opt(ea, i) == 'register')
 freg = lambda reg: lambda ea: any(reg.relatedQ(ins.op(ea, i)) for i in ins.ops_read(ea) if isinstance(ins.op(ea, i), symbol_t)) or any(reg.relatedQ(ins.op(ea, i)) for i in ins.ops_write(ea) if isinstance(ins.op(ea, i), symbol_t) and ins.opt(ea, i) == 'register') or any(any(r.relatedQ(reg) for r in ins.op(ea, i).symbols) for i in range(ins.ops_count(ea)) if ins.opt(ea, i) == 'phrase')
--- posix/.pythonrc.py
+++ posix/.pythonrc.py
@@ -13,69 +13,72 @@ if os.path.exists(builtins._): execfile(builtins._, {'__file__':builtins._})
 # add ~/.python/* to python module search path
 map(sys.path.append, __import__('glob').iglob(os.path.join(os.environ.get('HOME', os.path.expanduser('~')).replace('\\', os.sep).replace('/', os.sep), '.python', '*')))
 
-## some functional primitives in the default namespace
-# box any specified arguments
-fbox = fpack = lambda *a: a
-# return a closure that executes `f` with the arguments unboxed.
-funbox = funpack = lambda f, *a, **k: lambda *ap, **kp: f(*(a + functools.reduce(operator.add, builtins.map(builtins.tuple, ap), ())), **{ key : value for key, value in itertools.chain(k.items(), kp.items())})
-# return the first argument
-fcar = lambda *a: a[:1][0]
-# return the rest of the arguments
-fcdr = lambda *a: a[1:]
+## some functional programming combinators in the default namespace
+
+# return a closure that executes `F` with the arguments boxed and concatenated.
+fpack = lambda F, *a, **k: lambda *ap, **kp: F(a + ap, **{ key : value for key, value in itertools.chain(k.items(), kp.items()) })
+# return a closure that executes `F` with all of its arguments concatenated and unboxed.
+funpack = lambda F, *a, **k: lambda *ap, **kp: F(*(a + functools.reduce(operator.add, builtins.map(builtins.tuple, ap), ())), **{ key : value for key, value in itertools.chain(k.items(), kp.items()) })
+# return a closure that executes `F` with only its first argument.
+fcar = lambda F, *a, **k: lambda *ap, **kp: F(*(a + ap[:1]), **{ key : value for key, value in itertools.chain(k.items(), kp.items()) })
+# return a closure that executes `F` with all of it arguments but the first.
+fcdr = lambda F, *a, **k: lambda *ap, **kp: F(*(a + ap[1:]), **{ key : value for key, value in itertools.chain(k.items(), kp.items()) })
 # return a closure that will check that `object` is an instance of `type`.
 finstance = lambda *type: frpartial(builtins.isinstance, type)
 # return a closure that will check if its argument has an item `key`.
-fhasitem = fitemQ = lambda key: fcompose(fcatch(frpartial(operator.getitem, key)), builtins.iter, builtins.next, fpartial(operator.eq, None))
-# return a closure that will get a particular element from an object
+fhasitem = fitemQ = lambda key: frpartial(operator.contains, key)
+# return a closure that will get a particular element from an object.
 fgetitem = fitem = lambda item, *default: lambda object: default[0] if default and item not in object else object[item]
-# return a closure that will set a particular element on an object
+# return a closure that will set a particular element on an object.
 fsetitem = lambda item: lambda value: lambda object: operator.setitem(object, item, value) or object
+# return a closure that will remove a particular element from an object and return the modified object
+fdelitem = lambda *items: fcompose(fmap(fidentity, *[fcondition(fhasitem(item))(frpartial(operator.delitem, item), None) for item in items]), builtins.iter, builtins.next)
 # return a closure that will check if its argument has an `attribute`.
 fhasattr = fattributeQ = lambda attribute: frpartial(builtins.hasattr, attribute)
-# return a closure that will get a particular attribute from an object
+# return a closure that will get a particular attribute from an object.
 fgetattr = fattribute = lambda attribute, *default: lambda object: getattr(object, attribute, *default)
-# return a closure that will set a particular attribute on an object
+# return a closure that will set a particular attribute on an object.
 fsetattr = fsetattribute = lambda attribute: lambda value: lambda object: builtins.setattr(object, attribute, value) or object
 # return a closure that always returns `object`.
 fconstant = fconst = falways = lambda object: lambda *a, **k: object
-# a closure that returns its argument always
+# a closure that returns its argument always.
 fidentity = lambda object: object
 # a closure that returns a default value if its object is false-y
 fdefault = lambda default: lambda object: object or default
 # return the first, second, or third item of a box.
 first, second, third, last = operator.itemgetter(0), operator.itemgetter(1), operator.itemgetter(2), operator.itemgetter(-1)
-# return a closure that executes a list of functions one after another from left-to-right
-fcompose = lambda *f: functools.reduce(lambda f1, f2: lambda *a: f1(f2(*a)), builtins.reversed(f))
-# return a closure that executes function `f` whilst discarding any extra arguments
-fdiscard = lambda f: lambda *a, **k: f()
+# return a closure that executes a list of functions one after another from left-to-right.
+fcompose = lambda *Fa: functools.reduce(lambda F1, F2: lambda *a: F1(F2(*a)), builtins.reversed(Fa))
+# return a closure that executes function `F` whilst discarding any arguments passed to it.
+fdiscard = lambda F, *a, **k: lambda *ap, **kp: F(*a, **k)
 # return a closure that executes function `crit` and then returns/executes `f` or `t` based on whether or not it's successful.
 fcondition = lambda crit: lambda t, f: \
     lambda *a, **k: (t(*a, **k) if builtins.callable(t) else t) if crit(*a, **k) else (f(*a, **k) if builtins.callable(f) else f)
 # return a closure that takes a list of functions to execute with the provided arguments
-fmap = lambda *fa: lambda *a, **k: builtins.tuple(f(*a, **k) for f in fa)
-#lazy = lambda f, state={}: lambda *a, **k: state[(f, a, builtins.tuple(builtins.sorted(k.items())))] if (f, a, builtins.tuple(builtins.sorted(k.items()))) in state else state.setdefault((f, a, builtins.tuple(builtins.sorted(k.items()))), f(*a, **k))
-#lazy = lambda f, *a, **k: lambda *ap, **kp: f(*(a + ap), **{ key : value for key, value in itertools.chain(k.items(), kp.items())})
+fmap = lambda *Fa: lambda *a, **k: builtins.tuple(F(*a, **k) for F in Fa)
+#lazy = lambda F, state={}: lambda *a, **k: state[(F, a, builtins.tuple(builtins.sorted(k.items())))] if (F, a, builtins.tuple(builtins.sorted(k.items()))) in state else state.setdefault((F, a, builtins.tuple(builtins.sorted(k.items()))), F(*a, **k))
+#lazy = lambda F, *a, **k: lambda *ap, **kp: F(*(a + ap), **{ key : value for key, value in itertools.chain(k.items(), kp.items())})
 # return a memoized closure that's lazy and only executes when evaluated
-def flazy(f, *a, **k):
+def flazy(F, *a, **k):
     sortedtuple, state = fcompose(builtins.sorted, builtins.tuple), {}
     def lazy(*ap, **kp):
         A, K = a + ap, sortedtuple(builtins.tuple(k.items()) + builtins.tuple(kp.items()))
-        return state[(A, K)] if (A, K) in state else state.setdefault((A, K), f(*A, **{ key : value for key, value in itertools.chain(k.items(), kp.items())}))
+        return state[(A, K)] if (A, K) in state else state.setdefault((A, K), F(*A, **{ key : value for key, value in itertools.chain(k.items(), kp.items()) }))
     return lazy
 # return a closure with the function's arglist partially applied
 fpartial = functools.partial
-# return a closure that applies the provided arguments to the function `f`.
-fapply = lambda f, *a, **k: lambda *ap, **kp: f(*(a + ap), **{ key : value for key, value in itertools.chain(k.items(), kp.items())})
+# return a closure that applies the provided arguments to the function `F`.
+fapply = lambda F, *a, **k: lambda *ap, **kp: F(*(a + ap), **{ key : value for key, value in itertools.chain(k.items(), kp.items()) })
 # return a closure that will use the specified arguments to call the provided function.
-fcurry = lambda *a, **k: lambda f, *ap, **kp: f(*(a + ap), **{ key : value for key, value in itertools.chain(k.items(), kp.items())})
-# return a closure that applies the initial arglist to the end of function `f`.
-frpartial = lambda f, *a, **k: lambda *ap, **kp: f(*(ap + builtins.tuple(builtins.reversed(a))), **{ key : value for key, value in itertools.chain(k.items(), kp.items())})
-# return a closure that applies the arglist to function `f` in reverse.
-freverse = lambda f, *a, **k: lambda *ap, **kp: f(*builtins.reversed(a + ap), **{ key : value for key, value in itertools.chain(k.items(), kp.items())})
-# return a closure that executes function `f` and includes the caught exception (or None) as the first element in the boxed result.
-def fcatch(f, *a, **k):
+fcurry = lambda *a, **k: lambda F, *ap, **kp: F(*(a + ap), **{ key : value for key, value in itertools.chain(k.items(), kp.items()) })
+# return a closure that applies the initial arglist to the end of function `F`.
+frpartial = lambda F, *a, **k: lambda *ap, **kp: F(*(ap + builtins.tuple(builtins.reversed(a))), **{ key : value for key, value in itertools.chain(k.items(), kp.items()) })
+# return a closure that applies the arglist to function `F` in reverse.
+freverse = lambda F, *a, **k: lambda *ap, **kp: F(*builtins.reversed(a + ap), **{ key : value for key, value in itertools.chain(k.items(), kp.items()) })
+# return a closure that executes function `F` and includes the caught exception (or None) as the first element in the boxed result.
+def fcatch(F, *a, **k):
     def fcatch(*a, **k):
-        try: return None, f(*a, **k)
+        try: return None, F(*a, **k)
         except: return sys.exc_info()[1], None
     return functools.partial(fcatch, *a, **k)
 # boolean inversion of the result of a function
@@ -89,7 +92,7 @@ itake = lambda count: fcompose(builtins.iter, fmap(*[builtins.next] * count), bu
 # get the `nth` element from an iterator
 iget = lambda count: fcompose(builtins.iter, fmap(*[builtins.next] * count), builtins.tuple, operator.itemgetter(-1))
 # copy from itertools
-islice, imap, ifilter, ichain, izip, lzip = itertools.islice, fcompose(builtins.map, builtins.iter), fcompose(builtins.filter, builtins.iter), itertools.chain, fcompose(builtins.zip, builtins.iter), fcompose(builtins.zip, builtins.list)
+islice, imap, ifilter, ichain, izip = itertools.islice, fcompose(builtins.map, builtins.iter), fcompose(builtins.filter, builtins.iter), itertools.chain, fcompose(builtins.zip, builtins.iter)
 # restoration of the Py2-compatible list types
 lslice, lmap, lfilter, lzip = fcompose(itertools.islice, builtins.list), fcompose(builtins.map, builtins.list), fcompose(builtins.filter, builtins.list), fcompose(builtins.zip, builtins.list)
 # count number of elements of a container
