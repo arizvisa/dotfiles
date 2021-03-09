f0b9f0ef90095313c7471512362683b35d6dcd4f
Ali Rizvi-Santiago <arizvisa@gmail.com>
2021-03-09 15:44:15 -0600

    Updated the pythonrc files from the posix directory with a few minor changes.

--- posix/.idapythonrc.py
+++ posix/.idapythonrc.py
@@ -80,3 +80,48 @@ previous_written = fcompose(fmap(fidentity, fcompose(fmap(fpartial(fpartial, ins
 
 freg_written = lambda reg: lambda ea: any(reg.relatedQ(ins.op(ea, i)) for i in ins.ops_write(ea) if ins.opt(ea, i) == 'register')
 freg = lambda reg: lambda ea: any(reg.relatedQ(ins.op(ea, i)) for i in ins.ops_read(ea) if isinstance(ins.op(ea, i), symbol_t)) or any(reg.relatedQ(ins.op(ea, i)) for i in ins.ops_write(ea) if isinstance(ins.op(ea, i), symbol_t) and ins.opt(ea, i) == 'register') or any(any(r.relatedQ(reg) for r in ins.op(ea, i).symbols) for i in range(ins.ops_count(ea)) if ins.opt(ea, i) == 'phrase')
+
+def advise(name):
+ state = [0]
+ def current():
+  ea = ui.current.address()
+  return ea, ea
+ def selected():
+  return ui.current.selection()
+ def fixup(items, name, message):
+  res, item = next(((ea, db.tag(ea, name)) for ea in sorted(items) if name in db.tag(ea)), (None, None))
+  if res is None:
+   return state[0], len(items), message
+  return db.tag(res, name, None)
+ def tagger(bounds, message, state=state):
+  items = [ea for ea in db.address.iterate(bounds)]
+  count, _, _ = fixup(items, name, message)
+  db.tag(min(items) if items else bounds.left, name, (count, len(items), message))
+  return count
+ def ask(question):
+  return ui.ask.note(question)
+ def closure():
+  try:
+   bounds = selected()
+  except Exception:
+   bounds = current()
+  message = ask("note or message?")
+  if message:
+   tagger(bounds, message)
+   state[0] += 1
+  return
+ return closure
+
+def advisegdb(tag, outfile):
+ items = {}
+ for res in db.selectcontents(t):
+  for ea, res in func.select(*res):
+   idx, cnt, note = res[t]
+   items[idx] = ea, cnt, note
+  continue
+ with open(rp, 'wt') as outfile:
+  for idx in sorted(items):
+   ea, cnt, note = items[idx]
+   [ six.print_(r'echo # {!s}\n'.format(item.replace('\\', '\\\\')), file=outfile) for item in note.split('\n') ]
+   six.print_("x/{:d}i {:#x}".format(cnt, ea), file=outfile)
+ return outfile
