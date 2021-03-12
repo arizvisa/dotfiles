8a20834637babdea9793cf822aaa5d9a7451a94e
Ali Rizvi-Santiago <arizvisa@gmail.com>
2021-03-11 23:42:00 -0600

    Cleaned up the way the terminal capabilities are read by the bin/termcaps.sh script using ssam and tail.

--- posix/bin/termcaps.sh
+++ posix/bin/termcaps.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 mapfile terminfo < <( man -w 5 terminfo | xargs zcat | ssam -e 'x/T{/ .,/T}/ x/\n/ d' | grep 'T{\|T}' | ssam -e 'x/T{|T}/ s/T(.)/\1/')
-infocmp -1 | grep -ve '^#' | ( read n; cat ) | cut -d= -f1 | tr -cd '[:alpha:][:digit:]\n' | while read capability; do
-  comment=`printf "%s" "${terminfo[@]}" | grep "\b$capability[^[:print:]]" | cut -f4`
-  printf "%s -- %s\n" "$capability" "$comment"
+infocmp -1 | ssam -e 'x/[^     ]*#.*$\n/ v/[^  ]#/ d' | tail -n +2 | cut -f2 | ssam -e 'x/[,=]+.*/ d' | while read record; do
+  capability=`printf "%s\n" "$record" | cut -d# -f1`
+  comment=`printf "%s" "${terminfo[@]}" | grep "\b$capability\b" | cut -f4 | head -n 1`
+  printf "%s -- %s\n" "$record" "$comment"
 done
