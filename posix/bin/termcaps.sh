7ba370db61ceb5bc6d5c83541877d188a235147d
Ali Rizvi-Santiago <arizvisa@gmail.com>
2021-03-11 22:49:08 -0600

    Updated bin/termcaps.sh to use ssam when parsing troff output from the terminfo manpage.

--- posix/bin/termcaps.sh
+++ posix/bin/termcaps.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-infocmp -1 | grep -ve '^#' | ( read n; cat ) | cut -d= -f1 | tr -cd '[:alpha:][:digit:]\n' | while read n; do
-  comment=`man -w 5 terminfo | xargs zcat | egrep -A 2 "\b$n[^[:print:]]" | tr -d '\n' | sed 's/T{/{/g;s/T}/}/g' | egrep -m1 -o '\{[^}]+\}' | tr -d '\n'`
-  printf "%s -- %s\n" "$n" "$comment"
+infocmp -1 | grep -ve '^#' | ( read n; cat ) | cut -d= -f1 | tr -cd '[:alpha:][:digit:]\n' | while read capability; do
+  comment=`man -w 5 terminfo | xargs zcat | ssam -e 'x/T{/ .,/T}/ x/\n/ d' | grep 'T{\|T}' | grep "\b$capability[^[:print:]]" | ssam -e '/T{/,/T}/ x/T{|T}/ s/T(.)/\1/' | cut -f4`
+  printf "%s -- %s\n" "$capability" "$comment"
 done
