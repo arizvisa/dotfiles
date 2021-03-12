1b4203167783eef026524cdd9ce0cd92b7c488b6
Ali Rizvi-Santiago <arizvisa@gmail.com>
2021-03-11 22:54:44 -0600

    Updated bin/termcaps.sh to use bash to preload the terminfo database prior to grepping it for each individual capability.

--- posix/bin/termcaps.sh
+++ posix/bin/termcaps.sh
@@ -1,5 +1,6 @@
-#!/bin/sh
+#!/bin/bash
+mapfile terminfo < <( man -w 5 terminfo | xargs zcat | ssam -e 'x/T{/ .,/T}/ x/\n/ d' | grep 'T{\|T}' | ssam -e 'x/T{|T}/ s/T(.)/\1/')
 infocmp -1 | grep -ve '^#' | ( read n; cat ) | cut -d= -f1 | tr -cd '[:alpha:][:digit:]\n' | while read capability; do
-  comment=`man -w 5 terminfo | xargs zcat | ssam -e 'x/T{/ .,/T}/ x/\n/ d' | grep 'T{\|T}' | grep "\b$capability[^[:print:]]" | ssam -e '/T{/,/T}/ x/T{|T}/ s/T(.)/\1/' | cut -f4`
+  comment=`printf "%s" "${terminfo[@]}" | grep "\b$capability[^[:print:]]" | cut -f4`
   printf "%s -- %s\n" "$capability" "$comment"
 done
