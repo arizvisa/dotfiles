336073b8b4ac6a3a739fc8b11b02b2fafa3f0702
Ali Rizvi-Santiago <arizvisa@gmail.com>
2021-04-15 13:22:18 -0500

    Updated the build-idb64.sh and for-idb64.sh scripts under the posix/bin directory to search for their partners.

--- posix/bin/build-idb64.sh
+++ posix/bin/build-idb64.sh
@@ -1,4 +1,4 @@
 path=`dirname "$0"`
 arg0=`basename "$0"`
-prog="$HOME/bin/build-idb.sh"
+prog=`type -P "build-idb.sh"`
 exec -a "${path}/${arg0}" "${prog}" "$@"
--- posix/bin/for-idb64.sh
+++ posix/bin/for-idb64.sh
@@ -1,4 +1,4 @@
 path=`dirname "$0"`
 arg0=`basename "$0"`
-prog="$HOME/bin/for-idb.sh"
+prog=`type -P "for-idb.sh"`
 exec -a "${path}/${arg0}" "${prog}" "$@"
