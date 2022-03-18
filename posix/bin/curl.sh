f0903eba0166c44b736cac96978ec941a58b4d54
Ali Rizvi-Santiago <arizvisa@gmail.com>
2022-03-18 01:15:16 -0500

    Expanded the parameters passed to the bin/curl.sh script from the posix directory.

--- posix/bin/curl.sh
+++ posix/bin/curl.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-curl --silent -A "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.4 (KHTML, like Gecko) Chrome/22.0.1229.96 Safari/537.4" "$@"
+curl --silent --globoff --location --no-buffer --path-as-is --user-agent "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.4 (KHTML, like Gecko) Chrome/22.0.1229.96 Safari/537.4" "$@"
 
