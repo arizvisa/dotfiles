29b5cd226997f0d73e9d99c38ecc9b2187e41e6e
Ali Rizvi-Santiago <arizvisa@gmail.com>
2022-08-05 15:50:10 -0500

    Minor fixes to the curl-related posix tools to allow passing options via their environment variables.

--- posix/bin/curl.sh
+++ posix/bin/curl.sh
@@ -1,3 +1,12 @@
 #!/bin/sh
-curl --silent --globoff --location --no-buffer --path-as-is --user-agent "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.4 (KHTML, like Gecko) Chrome/22.0.1229.96 Safari/537.4" "$@"
+CURL_PARAMS=(--silent --globoff --location --no-buffer --path-as-is)
 
+HTTP_USER_AGENT=${HTTP_USER_AGENT:-'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.4 (KHTML, like Gecko) Chrome/22.0.1229.96 Safari/537.4'}
+CURL_PARAMS=("${CURL_PARAMS[@]}" --user-agent "$HTTP_USER_AGENT")
+
+# wtf, curl. why are these only available in libcurl?
+[ -z "$CURLOPT_COOKIE" ] ||     CURL_PARAMS=("${CURL_PARAMS[@]}" "-b $CURLOPT_COOKIE")
+[ -z "$CURLOPT_COOKIEFILE" ] || CURL_PARAMS=("${CURL_PARAMS[@]}" "-b $CURLOPT_COOKIEFILE")
+[ -z "$CURLOPT_COOKIEJAR" ] ||  CURL_PARAMS=("${CURL_PARAMS[@]}" "-c $CURLOPT_COOKIEJAR")
+
+curl "${CURL_PARAMS[@]}" "$@"
