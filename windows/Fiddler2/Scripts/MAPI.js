2aff3bf1535a3f8167602dfff512e5e953bfcc3f
Ali Rizvi-Santiago <arizvisa@gmail.com>
2023-04-01 23:59:37 -0500

    Added a couple of scripts from the https://github.com/OfficeDev/Office-Inspectors-for-Fiddler repository.

---
+++ windows/Fiddler2/Scripts/MAPI.js
@@ -0,0 +1,22 @@
+﻿/**
+  This is part of https://github.com/OfficeDev/Office-Inspectors-for-Fiddler/
+
+  It adds a column that can be used to identify MAPI and ROPS requests.
+*/
+public static BindUIColumn("MS Protocol")
+function CalcMethodCol(oS : Session) {
+    var isMAPI: boolean = false;
+    if (oS.RequestHeaders.ExistsAndContains("Content-Type", "application/mapi-http")) {
+        isMAPI = true;    } else if (oS.ResponseHeaders.ExistsAndEquals("X-ResponseCode", "0") && oS.ResponseHeaders.ExistsAndContains("Content-Type", "application/mapi-http")) {
+        isMAPI = true;    } else if (oS.ResponseHeaders.Exists("X-ResponseCode") && !oS.ResponseHeaders.ExistsAndEquals("X-ResponseCode", "0") && oS.ResponseHeaders.ExistsAndContains("Content-Type", "text/html")) {
+        isMAPI = true;
+    } else {
+        return "";
+    }
+    var sRequestType = oS.RequestHeaders["X-RequestType"];
+    switch (sRequestType) {
+        case "Execute":
+            return "MS-OXCROPS";        default:
+            return "MS-OXCMAPIHTTP";
+    }
+}
