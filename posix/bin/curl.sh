4baf46164af9b50c4d1c02db459853c7ca97671b
Ali Rizvi-Santiago <arizvisa@gmail.com>
2023-04-19 20:57:53 -0500

    Tweaked the bin/curl.sh posix script so that it discards the shell and execs into curl.

--- posix/bin/curl.sh
+++ posix/bin/curl.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-CURL_PARAMS=(--silent --globoff --location --no-buffer --path-as-is --anyauth)
+CURL_PARAMS=(--silent --show-error --globoff --location --no-buffer --path-as-is --anyauth)
 
 HTTP_USER_AGENT=${HTTP_USER_AGENT:-'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.4 (KHTML, like Gecko) Chrome/22.0.1229.96 Safari/537.4'}
 CURL_PARAMS=("${CURL_PARAMS[@]}" --user-agent "$HTTP_USER_AGENT")
@@ -9,4 +9,4 @@ CURL_PARAMS=("${CURL_PARAMS[@]}" --user-agent "$HTTP_USER_AGENT")
 [ -z "$CURLOPT_COOKIEFILE" ] || CURL_PARAMS=("${CURL_PARAMS[@]}" "-b $CURLOPT_COOKIEFILE")
 [ -z "$CURLOPT_COOKIEJAR" ] ||  CURL_PARAMS=("${CURL_PARAMS[@]}" "-c $CURLOPT_COOKIEJAR")
 
-curl "${CURL_PARAMS[@]}" "$@"
+exec curl "${CURL_PARAMS[@]}" "$@"
