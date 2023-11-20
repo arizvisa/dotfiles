e35ad4b14b39ec386c73161e4b9294770512ffa0
Ali Rizvi-Santiago <arizvisa@gmail.com>
2023-11-20 12:32:51 -0600

    Updated the "Developer Console Extensions" browser script to allow re-entrancy and to emit the contents of each closure prior to their actual execution.

--- browser/Developer Console Extensions/Developer Console Extensions.user.js
+++ browser/Developer Console Extensions/Developer Console Extensions.user.js
@@ -2,7 +2,7 @@
 // @name             Developer Console Extensions
 // @description      This adds a number of utilities to the developer console object
 // @version          1
-// @allframes        false
+// @allframes        true
 // @match            *://*/*
 // @run-at           document_end
 // @inject-into      page
@@ -19,10 +19,18 @@
 
 */
 
-const $DEBUG = false;
+var DEFAULTS = {
+  $DEBUG: false,
+  window: (typeof unsafeWindow === "undefined")? window.wrappedJSObject : unsafeWindow,
+};
 
-// because the author of FM is a fucking idiot...oh, and erosman/support#429.
-const GLOBAL = {};
+/// create a global variable and update it with the DEFAULTS if it doesn't exist yet.
+var GLOBAL;
+  if (typeof GLOBAL === "undefined") { GLOBAL = {}; }
+  for (let attribute in DEFAULTS) { GLOBAL[attribute] = DEFAULTS[attribute]; }
+DEFAULTS = undefined;
+
+/// because the author of FM is a fucking idiot...oh, and erosman/support#429.
 if (typeof GM === "undefined") {
   GLOBAL.info = {
     scriptHandler: "FireMonkeyIsWrittenByAFuckingIdiot",
@@ -36,44 +44,58 @@ if (typeof GLOBAL.info.script.uuid === "undefined") {
   GLOBAL.info.script.uuid = `GUID\$${window.crypto.randomUUID().replaceAll("-", "$")}`;
 }
 
-const private = ((info) => {
+/// object for providing an identifier unique to the script rather than the running instance.
+GLOBAL.private = ((info, private) => {
   const owner = info.scriptHandler;
   const uuid = info.script.uuid;
 
   let id = uuid.replace(/\W/g, '$');
   let name = `${owner}\$${id}`;
 
-  let variable = 0;
+  let variable = (typeof private === "undefined")? 0 : private.count();
   return {
     id: () => uuid,
     owner: () => owner,
     name: () => name,
+    count: () => variable,
     new: () => `\$${name}\$${variable++}`,
   };
-})(GLOBAL.info);
+})(GLOBAL.info, GLOBAL.private);
 
 /** main code **/
 function main(items) {
 
-  // aggregate an array of the necessary chunks for our closures
+  /// aggregate an array of the necessary chunks for our closures
   let res = [];
 
+  /// FireMonkey's editor, syntax highlighter, and even the damned linter are written by a fucking retard...
   for (let item of items) {
+    // res = res.concat(inject_closure(((owner, name) => console.info(`${owner}: attempting to attach following closure to "console.${name}"...`)), GLOBAL.private.name(), item.name));
+    res = res.concat(inject_closure(((owner, name) => console.info(owner + `: attempting to attach following closure to "` + ["console", name].join(".") + `"...`)), GLOBAL.private.name(), item.name));
+
+    // res = res.concat(inject_scope(((owner, closure) => console.info(`${owner}: ${toSource(closure)}`)), {toSource: toSource}, GLOBAL.private.name(), item.closure));
+    res = res.concat(inject_scope(((owner, closure) => console.info(owner + `: ` + toSource(closure))), {toSource: toSource}, GLOBAL.private.name(), item.closure));
+
+    /// i fucking love how JS linters complain about the dumbest shit (i.e. previous line gets a complaint about potentially confusing semantics).
+    /// it is pretty damned obvious that JS programmers fucking suck at writing code if storing your scope in an object is considered confusing.
     res = res.concat(setattr_console(item.name, item.closure));
+
+    // res = res.concat(inject_closure(((owner, name) => console.info(`${owner}: successfully attached closure to "console.${name}".`)), GLOBAL.private.name(), item.name));
+    res = res.concat(inject_closure(((owner, name) => console.info(owner + `: successfully attached closure to "` + ["console", name].join(".") + `".`)), GLOBAL.private.name(), item.name));
   }
 
-  // add some other attributes to the console object
+  /// add some other attributes to the console object
   res = res.concat(setattr_console("state", {}));
 
-  // convert these chunks into solid text objects that we'll append
+  /// convert these chunks into solid text objects that we'll append
   let chunks = res.map(item => new Text(item));
 
-  // create a script object and append all of our text items
+  /// create a script object and append all of our text items
   const script = document.createElement('script', { type: "text/javascript" });
-  script.dataset.creator = private.owner();
-  script.dataset.owner = private.id();
+  script.dataset.creator = GLOBAL.private.owner();
+  script.dataset.owner = GLOBAL.private.id();
 
-  if ($DEBUG) {
+  if (GLOBAL.$DEBUG) {
     console.info(`Created ${script.toString()} with creator "${script.dataset.creator}" owned by ${script.dataset.owner}`);
 
     for (let index = 0; index < chunks.length; index++) {
@@ -83,13 +105,13 @@ function main(items) {
 
   chunks.forEach(chunk => script.appendChild(chunk));
 
-  // inject the closure to remove our script element when we're done
+  /// inject the closure to remove our script element when we're done
   const Fcleanup = (creator, owner) => document.querySelectorAll(`script[data-creator="${creator}"][data-owner="${owner}"]`).forEach(E => E.remove());
 
   let closures = inject_closure(Fcleanup, script.dataset.creator, script.dataset.owner);
 
-  if ($DEBUG) {
-    console.info(`Injecting cleanup closure for ${script.toString()} with selector: ${script.nodeName}[data-creator="${script.dataset.creator}"][data-owner="${script.dataset.owner}"]`);
+  if (GLOBAL.$DEBUG) {
+    console.info(`Created cleanup closure for ${script.toString()} with selector: ${script.nodeName}[data-creator="${script.dataset.creator}"][data-owner="${script.dataset.owner}"]`);
 
     for (let index = 0; index < closures.length; index++) {
       console.debug(`Line #${index}: ${closures[index]}`);
@@ -98,12 +120,12 @@ function main(items) {
 
   closures.forEach(chunk => script.appendChild(new Text(chunk)));
 
-  // finally we can attach it
+  /// finally we can attach it to the document
   (document.body || document.head || document.documentElement).appendChild(script);
 }
 
 function setattr_console(attribute, closure) {
-  const varname = private.new();
+  const varname = GLOBAL.private.new();
 
   let setattribute = (attribute, value) => { window.console[attribute] = value; };
 
@@ -115,7 +137,7 @@ function setattr_console(attribute, closure) {
 }
 
 function inject_closure(closure, ...parameters) {
-  const varname = private.new();
+  const varname = GLOBAL.private.new();
 
   let res = [];
   res.push(`${varname} = ${toSource(closure)};`);
@@ -124,11 +146,26 @@ function inject_closure(closure, ...parameters) {
   return res;
 }
 
+function inject_scope(closure, scope, ...parameters) {
+  const varname = GLOBAL.private.new();
+
+  let res = [];
+  res.push(`{`);
+    for (let name in scope) {
+      res.push(`let ${name} = ${toSource(scope[name])};`);
+    }
+    res.push(`${varname} = ${toSource(closure)};`);
+    res.push(`${varname}.apply(${undefined}, ${toSource(parameters)});`);
+    res.push(`delete(${varname});`);
+  res.push(`}`);
+  return res;
+}
+
 /** functions to load into the document **/
-const TestAttributeAssignment = () => {
+var TestAttributeAssignment = () => {
   console.warn('Successfully assigned the defined attributes!');
 
-  // collect all of the attributes currently assigned to the console
+  /// collect all of the attributes currently assigned to the console
   let res = [];
   for (let item in console) {
     res.push(item);
@@ -138,12 +175,12 @@ const TestAttributeAssignment = () => {
   console.debug(res.join(', '));
 };
 
-const DownloadJSONBlob = (object, filename) => {
+var DownloadJSONBlob = (object, filename) => {
   const DefaultTabSize = 4;
   const DefaultMimeType = "text/json";
   const DefaultFilename = "console.json";
 
-  // check parameters
+  /// check parameters
   if (object === undefined) {
     console.error(`DownloadJSONBlob: refusing to encode undefined value (${object})`);
     return false;
@@ -157,12 +194,12 @@ const DownloadJSONBlob = (object, filename) => {
     console.info(`DownloadJSONBlob: using specified filename ${filename}`);
   }
 
-  // encode the object into a json blob
+  /// encode the object into a json blob
   var encoded = JSON.stringify(object, undefined, DefaultTabSize);
   const blob = new Blob([encoded], { type: DefaultMimeType });
   console.info(`DownloadJSONBlob: successfully encoded object into ${blob.size} bytes`);
 
-  // create a fake link that we can simulate a click with
+  /// create a fake link that we can simulate a click with
   const anchor = document.createElement('a');
 
   anchor.dataset.url = URL.createObjectURL(blob);
@@ -179,8 +216,8 @@ const DownloadJSONBlob = (object, filename) => {
   return true;
 };
 
-// because apparently the Object.prototype.toSource method is fucking Firefox specific...
-const toSource = (object) => {
+/// because apparently the Object.prototype.toSource method is fucking Firefox specific...
+var toSource = (object) => {
   switch (typeof(object)) {
 
   case "undefined":
@@ -213,16 +250,16 @@ const toSource = (object) => {
   }
 };
 
-const desired_attributes = () => {
+var desired_attributes = () => {
   let items = [];
 
-  // load a number of attributes that we want attached to the console
+  /// load a number of attributes that we want attached to the console
   items.push({name: "save", closure: DownloadJSONBlob});
   items.push({name: "export", closure: DownloadJSONBlob});
   items.push({name: "download", closure: DownloadJSONBlob});
   items.push({name: "toSource", closure: toSource});
 
-  if ($DEBUG) {
+  if (GLOBAL.$DEBUG) {
     items.push({name: "test", closure: TestAttributeAssignment});
   }
 
