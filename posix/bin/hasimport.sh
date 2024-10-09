ca166109b14cb693042aeb6247111bc2d1089c29
Ali Rizvi-Santiago <arizvisa@gmail.com>
2024-10-09 10:36:23 -0500

    Updated the bin/hasimport.sh posix script to ensure that it works despite its potential worthlessness.

--- posix/bin/hasimport.sh
+++ posix/bin/hasimport.sh
@@ -1,9 +1,25 @@
 #!/bin/sh
+if [ $# -lt 2 ] || ! tty -s; then
+    echo "usage: $0 program regex..." 1>&2
+    exit 22 # EINVAL
+fi
+
 program=$1
 shift
-symbols=$(echo "$@" | sed 's/ /\\\|/g')
-if test $(file -L -b --mime-type "$program") = "application/x-executable" && readelf -Wr "$program" | grep -e "$symbols" >/dev/null ; then
-    echo "$program"
-    exit 1
-fi
-exit 0
+symbols=`printf '%s\n' "$@" | sed 's/\n/\\\|/g'`
+
+mime=`file -L -b --mime-type "$program"`
+case $mime in
+    application/x-executable|application/x-pie-executable)
+        ok=3    # ESRCH
+        while IFS=' ' read offset info type value symbol; do
+            printf '%s\t%#x\t%s\t%#x\t%s\n' "$program" "0x$offset" "$type" "0x$value" "$symbol"
+            ok=0
+        done < <( readelf -Wr "$program" | grep -e ' R' | grep -e "$symbols" )
+        exit $ok
+    ;;
+    *)
+        echo "$0: unknown mime type: $mime" 1>&2
+    ;;
+esac
+exit 8 # ENOEXEC
