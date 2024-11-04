3071289c9c2c256714b1555e43d2263b4fdbc653
Ali Rizvi-Santiago <arizvisa@gmail.com>
2024-11-04 09:52:51 -0600

    Updated the swi-prolog initialization dotfile to correct the package urls.

--- posix/.config/swi-prolog/init.pl
+++ posix/.config/swi-prolog/init.pl
@@ -1,26 +1,34 @@
 :- use_module(library(prolog_pack)).
 
-%- http://packs.ndrix.com/func/index.html
+%- https://www.swi-prolog.org/pack/list?p=func
+:- not(prolog_pack:current_pack(function_expansion)) -> pack_install(function_expansion); true.
+:- not(prolog_pack:current_pack(list_util)) -> pack_install(list_util); true.
 :- not(prolog_pack:current_pack(func)) -> pack_install(func); true.
 :- use_module(library(func)).
 
-%- http://packs.ndrix.com/spawn/index.html
+%- https://www.swi-prolog.org/pack/list?p=spawn
 :- not(prolog_pack:current_pack(spawn)) -> pack_install(spawn); true.
 :- use_module(library(spawn)).
 
-%- http://packs.ndrix.com/mavis/index.html
+%- https://www.swi-prolog.org/pack/list?p=mavis
+:- not(prolog_pack:current_pack(list_util)) -> pack_install(list_util); true.
 :- not(prolog_pack:current_pack(quickcheck)) -> (format("WARNING: Do not run the post-installation scripts for ~w~n", [quickcheck]), pack_install(quickcheck)); true.
 :- not(prolog_pack:current_pack(mavis)) -> pack_install(mavis); true.
 :- use_module(library(mavis)).
 
 %- https://www.swi-prolog.org/pack/list?p=dcgutils
+:- not(prolog_pack:current_pack(genutils)) -> pack_install(genutils); true.
 :- not(prolog_pack:current_pack(dcgutils)) -> pack_install(dcgutils); true.
 %:- use_module(library(dcg_core)).
 :- use_module(library(dcg_progress)).
 
-%- https://github.com/kamahen/edcg
+%- https://www.swi-prolog.org/pack/list?p=edcg
 :- not(prolog_pack:current_pack(edcg)) -> pack_install(edcg); true.
 :- use_module(library(edcg)).
 
-%- https://github.com/jamesnvc/lsp_server
+%- https://github.com/ptarau/AnswerStreamGenerators/lazy_streams-0.5.0
+:- not(prolog_pack:current_pack(lazy_streams)) -> pack_install(lazy_streams, [url('https://github.com/arizvisa/ptarau-lazy_streams.git'), git(true), version('0.5.0')]); true.
+%:- use_module(library(lazy_streams)).
+
+%- https://www.swi-prolog.org/pack/list?p=lsp_server
 %:- not(prolog_pack:current_pack(lsp_server)) -> pack_install(lsp_server); true.
